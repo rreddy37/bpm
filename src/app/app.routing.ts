87bba2d324151444a4e639e815c9eef8f39e5f3e
rreddy37 <rammohan.greddy@gmail.com>
2018-09-28 11:15:46 +0530

    routing changes...

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,8 +1,6 @@
 import { Routes, RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
 import { BatchjobsComponent } from './batchjobs/batchjobs.component';
 
 const appRoutes: Routes = [
@@ -20,7 +18,7 @@ const appRoutes: Routes = [
     component: BatchjobsComponent
   },
   // otherwise redirect to home
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: '/home' }
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
